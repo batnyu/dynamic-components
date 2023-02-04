b3f6cd13d8b7f165d1d8bc6c72fa0d82bc9637bf
batnyu <vrignaudbaptiste@gmail.com>
2023-02-04 23:09:17 +0100

    refactor(widget-image): better import, ngIf if no src

--- libs/widgets/widget-image/ui/src/lib/widgets-widget-image-ui/widgets-widget-image-ui.component.ts
+++ libs/widgets/widget-image/ui/src/lib/widgets-widget-image-ui/widgets-widget-image-ui.component.ts
@@ -4,14 +4,14 @@ import {
   Input,
   OnInit,
 } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgIf } from '@angular/common';
 import type { WidgetImage } from '@test-widgets/widget-image-model';
 import type { AdComponent } from '@test-widgets/shared-utils';
 
 @Component({
   selector: 'test-widgets-widgets-widget-image-ui',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgIf],
   templateUrl: './widgets-widget-image-ui.component.html',
   styleUrls: ['./widgets-widget-image-ui.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
