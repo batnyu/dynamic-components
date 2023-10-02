9cdb44882cfb6f7e9cfc5abfea5b889cebbbf54d
batnyu <vrignaudbaptiste@gmail.com>
2023-10-02 09:26:23 +0200

    photo from assets, add optimization and chrome 81

--- libs/widget-assembler/src/lib/slide.service.ts
+++ libs/widget-assembler/src/lib/slide.service.ts
@@ -101,7 +101,7 @@ export class SlideService {
         {
           kind: 'image',
           data: {
-            src: 'https://images.unsplash.com/photo-1667561420668-3441555cabc9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80',
+            src: 'assets/image/photo.jpg',
             objectFit: 'contain',
             backgroundColor: 'orange',
           },
